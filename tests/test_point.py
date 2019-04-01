0a5805b2ee0d8959cb022dc5f7d8934181c2f593
Anirudha Bose <ani07nov@gmail.com>
2019-04-02 02:05:51 +0530

    Add unit test for point addition of vertical tangent case

--- tests/test_point.py
+++ tests/test_point.py
@@ -8,7 +8,7 @@ def test_ne():
     assert not (a != a)
 
 
-def test_add__x1_equals_x2():
+def test_add__additive_inverse():
     a = Point(x=None, y=None, a=5, b=7)
     b = Point(x=2, y=5, a=5, b=7)
     c = Point(x=2, y=-5, a=5, b=7)
@@ -17,12 +17,17 @@ def test_add__x1_equals_x2():
     assert b + c == a
 
 
-def test_add__x1_not_equals_x2():
+def test_add__line_with_slope():
     a = Point(x=3, y=7, a=5, b=7)
     b = Point(x=-1, y=-1, a=5, b=7)
     assert a + b == Point(x=2, y=-5, a=5, b=7)
 
 
-def test_add__p1_equals_p2():
+def test_add__tangent_with_slope():
     a = Point(x=-1, y=1, a=5, b=7)
     assert a + a == Point(x=18, y=-77, a=5, b=7)
+
+
+def test_add__vertical_tangent():
+    a = Point(x=3, y=0, a=-9, b=0)
+    assert a + a == Point(x=None, y=None, a=-9, b=0)
