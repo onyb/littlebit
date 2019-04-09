eb2f24b3be69a38cf9a043a30d5442605ee1b91e
Anirudha Bose <ani07nov@gmail.com>
2019-04-09 06:10:34 +0530

    Add test cases for compressed and uncompressed pubkeys in SEC format

--- tests/ecc/test_secp256k1.py
+++ tests/ecc/test_secp256k1.py
@@ -62,3 +62,29 @@ def test_sign():
     z = randint(0, 2 ** 256)
     sig = pk.sign(z)
     assert pk.point.verify(z, sig)
+
+
+def test_sec():
+    inputs = (
+        # coefficient, uncompressed sec, compressed sec
+        (
+            999 ** 3,
+            "049d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d56fa15cc7f3d38cda98dee2419f415b7513dde1301f8643cd9245aea7f3f911f9",
+            "039d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d5",
+        ),
+        (
+            123,
+            "04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b",
+            "03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5",
+        ),
+        (
+            42424242,
+            "04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3",
+            "03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e",
+        ),
+    )
+
+    for coefficient, uncompressed, compressed in inputs:
+        point = coefficient * G
+        assert point.sec(compressed=False) == bytes.fromhex(uncompressed)
+        assert point.sec(compressed=True) == bytes.fromhex(compressed)
