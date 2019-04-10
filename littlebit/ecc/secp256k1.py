edb0905977c431f9866b4af7db879c2dc05172e9
Anirudha Bose <ani07nov@gmail.com>
2019-04-10 21:43:40 +0530

    Implement serialization of signatures to DER binary format

--- littlebit/ecc/secp256k1.py
+++ littlebit/ecc/secp256k1.py
@@ -53,6 +53,47 @@ class Signature:
     def __repr__(self):
         return f"Signature(r={self.r:x}, s={self.s:x})"
 
+    def der(self) -> bytes:
+        # 1. Start with the 0x30 byte.
+        # 2. Append the length of the rest of the signature.
+        # 3. Append the marker byte, 0x02.
+        # 4. Encode r as a big-endian integer, but prepend it with the 0x00 byte if r's
+        #    first byte >= 0x80. Prepend the resulting length to r. Add this to the
+        #    result.
+        # 5. Append the marker byte, 0x02.
+        # 6. Encode s as a big-endian integer, but prepend it with the 0x00 byte if s's
+        #    first byte >= 0x80. Prepend the resulting length to s. Add this to the
+        #    result.
+
+        PREFIX = 0x30
+        MARKER = 0x02
+        NULL_BYTE = b"\x00"
+
+        # Serialze 256-bit integer to bytes with big-endian byteorder, and strip leading
+        # null bytes
+        rbin = self.r.to_bytes(32, byteorder="big").lstrip(NULL_BYTE)
+
+        # We check if rbin is negative, by verifying if the first bit of rbin is 1.
+        # This is brought about by doing a bitwise AND between the first bit of rbin
+        # and (10000000)₂ or (0x80)₁₆.
+        if rbin[0] & 0x80:
+            rbin = NULL_BYTE + rbin
+
+        result = bytes([MARKER, len(rbin)]) + rbin
+
+        # Serialze 256-bit integer to bytes with big-endian byteorder, and strip leading
+        # null bytes
+        sbin = self.s.to_bytes(32, byteorder="big").lstrip(NULL_BYTE)
+
+        # We check if sbin is negative, by verifying if the first bit of sbin is 1.
+        # This is brought about by doing a bitwise AND between the first bit of sbin
+        # and (10000000)₂ or (0x80)₁₆.
+        if sbin[0] & 0x80:
+            sbin = NULL_BYTE + sbin
+        result += bytes([MARKER, len(sbin)]) + sbin
+
+        return bytes([PREFIX, len(result)]) + result
+
 
 class S256Point(Point):
     def __init__(self, x, y, a=None, b=None):
