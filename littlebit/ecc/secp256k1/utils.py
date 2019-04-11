d0a00bec25249b72e8c161acf629ad38d0484bf3
Anirudha Bose <ani07nov@gmail.com>
2019-04-11 07:25:24 +0530

    Add some helpers to convert between little endian bytes and integers

--- littlebit/ecc/secp256k1/utils.py
+++ littlebit/ecc/secp256k1/utils.py
@@ -56,3 +56,17 @@ def encode_base58_checksum(bytestring: bytes) -> str:
     encoded into base58.
     """
     return encode_base58(bytestring + checksum(bytestring))
+
+
+def little_endian_to_int(bytestring: bytes):
+    """
+    Takes byte sequence as a little-endian number, and returns an integer
+    """
+    return int.from_bytes(bytestring, "little")
+
+
+def int_to_little_endian(n, length):
+    """
+    Takes an integer and returns the little-endian byte sequence of length
+    """
+    return n.to_bytes(length, "little")
