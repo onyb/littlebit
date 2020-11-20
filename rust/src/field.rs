5b2ad61c05566b58d7b6b5b81ce5681ee03ecfc5
Thomas Wilgenbus <thomas.wilgenbus-ext@ledger.fr>
2020-11-20 14:37:32 +0100

    Update field API

--- rust/src/field.rs
+++ rust/src/field.rs
@@ -6,7 +6,7 @@ construct_uint! {
     pub struct uint256(4);
 }
 
-#[derive(Eq, PartialEq, Debug)]
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
 pub struct FieldElement {
     pub number: uint256,
     pub prime: uint256,
@@ -22,23 +22,22 @@ impl FieldElement {
             ))
         } else {
             Ok(Self {
-                number: number,
-                prime: prime,
+                number,
+                prime,
             })
         }
     }
 
     // ? [FIXME] - What's the standard way of dealing with negative exponents?
     //             Maybe use something other than uint256?
-    pub fn pow(self, exponent: uint256, is_positive: bool) -> Result<Self, String> {
-        match is_positive {
-            true => Self::new(self.number.pow(exponent) % self.prime, self.prime),
-            false => {
-                let modulo = self.prime - uint256::one();
-                let n = (modulo - exponent) % modulo;
-                Self::new(self.number.pow(n) % self.prime, self.prime)
-            }
-        }
+    pub fn pow(self, exponent: uint256) -> Result<Self, String> {
+        Self::new(self.number.pow(exponent) % self.prime, self.prime)
+    }
+
+    pub fn neg_pow(self, exponent: uint256) -> Result<Self, String> {
+        let modulo = self.prime - uint256::one();
+        let n = (modulo - exponent) % modulo;
+        self.pow(n)
     }
 }
 
@@ -96,10 +95,8 @@ impl Div<FieldElement> for FieldElement {
              * means:
              *   1/n == nᵖ⁻²
              */
-            match other.pow(self.prime - uint256::one() - uint256::one(), true) {
-                Err(e) => Err(e),
-                Ok(el) => self * el,
-            }
+            other.pow(self.prime - uint256::one() - uint256::one())
+                .map(|el| -> self * el)
         }
     }
 }
@@ -107,12 +104,13 @@ impl Div<FieldElement> for FieldElement {
 impl Mul<FieldElement> for i64 {
     type Output = Result<FieldElement, String>;
     fn mul(self, element: FieldElement) -> Self::Output {
-        match self >= 0 {
-            true => FieldElement::new(
+        if self >= 0 {
+            FieldElement::new(
                 (element.number * uint256::from(self)) % element.prime,
                 element.prime,
-            ),
-            false => FieldElement::new(
+            )
+        } else {
+            FieldElement::new(
                 // -n x FieldElement, is same as:
                 //   P -  (n x FieldElement) % P
                 //
@@ -120,7 +118,7 @@ impl Mul<FieldElement> for i64 {
                 //   0 <= (n x FieldElement) % P < P
                 element.prime - ((element.number * uint256::from(self.abs())) % element.prime),
                 element.prime,
-            ),
+            )
         }
     }
 }
@@ -131,136 +129,137 @@ mod test {
     use super::FieldElement;
 
     #[test]
-    fn range() {
-        assert!(FieldElement::new(uint256::from(3), uint256::from(4)).is_ok(),);
-        assert!(FieldElement::new(uint256::from(7), uint256::from(3)).is_err(),);
+    fn constructor_when_number_smaller_than_prime_should_work() -> Result<FieldElement, String> {
+        FieldElement::new(uint256::from(3), uint256::from(4))
     }
 
     #[test]
-    fn equality() {
-        // ? [FIXME] - Possible to use keyword arguments?
-        let a = FieldElement::new(uint256::from(2), uint256::from(31));
-        let b = FieldElement::new(uint256::from(2), uint256::from(31));
-        let c = FieldElement::new(uint256::from(15), uint256::from(31));
-
-        assert_eq!(a, b);
-        assert_ne!(a, c);
-        assert_eq!(a == b, true);
+    fn constructor_when_number_bigger_than_prime_should_not_work() -> Result<FieldElement, String> {
+        FieldElement::new(uint256::from(7), uint256::from(3))
     }
 
     #[test]
-    fn addition() {
-        assert!(
-            (FieldElement::new(uint256::from(2), uint256::from(31)).unwrap()
-                + FieldElement::new(uint256::from(2), uint256::from(32)).unwrap())
-            .is_err()
-        );
+    fn equality_when_numbers_and_primes_are_equal_should_be_true() {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31));
+        let field_b = FieldElement::new(uint256::from(2), uint256::from(31));
 
-        assert_eq!(
-            (FieldElement::new(uint256::from(2), uint256::from(31)).unwrap()
-                + FieldElement::new(uint256::from(15), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(17), uint256::from(31))
-        );
-
-        assert_eq!(
-            (FieldElement::new(uint256::from(17), uint256::from(31)).unwrap()
-                + FieldElement::new(uint256::from(21), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(7), uint256::from(31))
-        );
+        assert_eq!(field_a, field_b);
     }
 
     #[test]
-    fn subtraction() {
-        assert!(
-            (FieldElement::new(uint256::from(2), uint256::from(31)).unwrap()
-                - FieldElement::new(uint256::from(2), uint256::from(32)).unwrap())
-            .is_err()
-        );
+    fn equality_when_numbers_and_primes_are_equal_should_be_true() {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31));
+        let field_c = FieldElement::new(uint256::from(15), uint256::from(31));
 
-        assert_eq!(
-            (FieldElement::new(uint256::from(29), uint256::from(31)).unwrap()
-                - FieldElement::new(uint256::from(4), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(25), uint256::from(31))
-        );
-
-        assert_eq!(
-            (FieldElement::new(uint256::from(15), uint256::from(31)).unwrap()
-                - FieldElement::new(uint256::from(30), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(16), uint256::from(31))
-        );
+        assert_ne!(field_a, field_c);
     }
 
     #[test]
-    fn multiplication() {
-        assert!(
-            (FieldElement::new(uint256::from(2), uint256::from(31)).unwrap()
-                * FieldElement::new(uint256::from(2), uint256::from(32)).unwrap())
-            .is_err()
-        );
-
-        assert_eq!(
-            (FieldElement::new(uint256::from(24), uint256::from(31)).unwrap()
-                * FieldElement::new(uint256::from(19), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(22), uint256::from(31))
-        );
+    fn addition_when_fields_have_different_primes_should_not_work() -> Result<FieldElement, Err> {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(2), uint256::from(32)).unwrap();
+        field_a + field_b
     }
 
     #[test]
-    fn exponentiation() {
-        assert_eq!(
-            FieldElement::new(uint256::from(17), uint256::from(31))
-                .unwrap()
-                .pow(uint256::from(3), true),
-            FieldElement::new(uint256::from(15), uint256::from(31))
-        );
-
-        assert_eq!(
-            FieldElement::new(uint256::from(5), uint256::from(31))
-                .unwrap()
-                .pow(uint256::from(5), true),
-            FieldElement::new(uint256::from(25), uint256::from(31))
-        );
-
-        assert_eq!(
-            FieldElement::new(uint256::from(17), uint256::from(31))
-                .unwrap()
-                .pow(uint256::from(3), false),
-            FieldElement::new(uint256::from(29), uint256::from(31))
-        );
-
-        assert_eq!(
-            FieldElement::new(uint256::from(4), uint256::from(31))
-                .unwrap()
-                .pow(uint256::from(4), false),
-            FieldElement::new(uint256::from(4), uint256::from(31))
-        );
+    fn addition_when_fields_have_same_primes_should_add_numbers() {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(15), uint256::from(31)).unwrap();
+        let field_c = FieldElement::new(uint256::from(13), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(17), uint256::from(31));
+        assert_eq!(field_a + field_b, expected);
     }
 
     #[test]
-    fn division() {
-        assert!(
-            (FieldElement::new(uint256::from(2), uint256::from(31)).unwrap()
-                * FieldElement::new(uint256::from(2), uint256::from(32)).unwrap())
-            .is_err()
-        );
-
-        assert_eq!(
-            (FieldElement::new(uint256::from(3), uint256::from(31)).unwrap()
-                / FieldElement::new(uint256::from(24), uint256::from(31)).unwrap()),
-            FieldElement::new(uint256::from(4), uint256::from(31))
-        );
+    fn addition_when_fields_have_same_primes_but_result_bigger_than_prime_should_add_numbers_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(17), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(21), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(7), uint256::from(31));
+        assert_eq!(field_a + field_b, expected);
     }
 
     #[test]
-    fn scalar_multiplication() {
-        assert_eq!(
-            5 * FieldElement::new(uint256::from(18), uint256::from(31)).unwrap(),
-            FieldElement::new(uint256::from(28), uint256::from(31))
-        );
+    fn subtraction_when_fields_have_different_primes_should_not_work() -> Result<FieldElement, String> {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(2), uint256::from(32)).unwrap();
+        field_a - field_b
+    }
 
-        assert_eq!(
-            -4 * FieldElement::new(uint256::from(11), uint256::from(31)).unwrap(),
-            FieldElement::new(uint256::from(18), uint256::from(31))
-        );
+    #[test]
+    fn subtraction_when_fields_have_same_primes_should_subtract_numbers() {
+        let field_a = FieldElement::new(uint256::from(29), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(4), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(25), uint256::from(31));
+        assert_eq!(field_a - field_b, expected);
+    }
+
+    #[test]
+    fn subtraction_when_fields_have_same_primes_should_subtract_numbers_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(15), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(30), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(16), uint256::from(31));
+        assert_eq!((field_a - field_b), expected);
+    }
+
+    #[test]
+    fn multiplication_when_fields_have_different_primes_should_not_work() -> Result<FieldElement, String> {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(2), uint256::from(32)).unwrap();
+        field_a * field_b
+    }
+
+    #[test]
+    fn multiplication_when_fields_have_same_primes_should_multiply_numbers_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(24), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(19), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(22), uint256::from(31));
+        assert_eq!(field_a * field_b, expected);
+    }
+
+    #[test]
+    fn exponentiation_when_fields_have_same_primes_should_raise_number_to_exponent_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(17), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(15), uint256::from(31));
+        assert_eq!(field_a.pow(uint256::from(3)), expected);
+
+        let field_a = FieldElement::new(uint256::from(5), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(25), uint256::from(31));
+        assert_eq!(field_a.pow(uint256::from(5)), expected);
+
+        let expected = FieldElement::new(uint256::from(29), uint256::from(31));
+        assert_eq!(field_a.neg_pow(uint256::from(3)), expected);
+
+        let field_a = FieldElement::new(uint256::from(4), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(4), uint256::from(31));
+        assert_eq!(field_a.neg_pow(uint256::from(4)), expected);
+    }
+
+    #[test]
+    fn division_when_fields_have_different_primes_should_not_work() -> Result<FieldElement, String> {
+        let field_a = FieldElement::new(uint256::from(2), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(2), uint256::from(32)).unwrap();
+        field_a / field_b
+    }
+
+    #[test]
+    fn division_when_fields_have_same_primes_should_divide_numbers_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(3), uint256::from(31)).unwrap();
+        let field_b = FieldElement::new(uint256::from(24), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(4), uint256::from(31));
+        assert_eq!(field_a / field_b, expected);
+    }
+
+    #[test]
+    fn scalar_multiplication_with_positive_scalar_should_multiple_number_to_scalar_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(18), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(28), uint256::from(31));
+        assert_eq!(5 * field_a, expected);
+    }
+
+    #[test]
+    fn scalar_multiplication_with_negative_scalar_should_multiple_number_to_scalar_modulo_prime() {
+        let field_a = FieldElement::new(uint256::from(11), uint256::from(31)).unwrap();
+        let expected = FieldElement::new(uint256::from(18), uint256::from(31));
+        assert_eq!(-4 * field_a, expected);
     }
 }
